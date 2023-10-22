ef2c5282a0485ef132d13ab6711c87c4281e4e48
David Porter <david@daporter.net>
2023-10-22 12:38:18 +0200

    Update colorscheme to Nord
    Mostly copied nathanielevan’s config:
    
      https://github.com/nathanielevan/dotfiles/tree/master

--- config.def.h
+++ config.def.h
@@ -12,11 +12,15 @@ static const int vertpadbar         = 4;        /* vertical padding for statusba
 static const char *fonts[]          = { "Iosevka Nerd Font Propo:pixelsize=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Iosevka Nerd Font Propo:pixelsize=11:antialias=true:autohint=true";
 static const char dmenuprompt[]     = "run:";
+static const char* const nord[]     = { "#2e3440", "#3b4252", "#434c5e", "#4c566a",
+                                        "#d8dee9", "#e5e9f0", "#eceff4",
+                                        "#8fbcbb", "#88c0d0", "#81a1c1", "#5e81ac",
+                                        "#bf616a", "#d08770", "#ebcb8b", "#a3be8c", "#b48ead" };
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { "#d4be98", "#282828", "#45403d" },
-	[SchemeSel]  = { "#282828", "#d8a657", "#d8a657" },
-	[SchemeUrg]  = { "#282828", "#ea6962", "#ea6962" },
+	[SchemeNorm] = { nord[4], nord[0],  nord[0]   },
+	[SchemeSel]  = { nord[0], nord[9],  nord[9]   },
+	[SchemeUrg]  = { nord[0], nord[11], nord[11]  },
 };
 
 /* tagging */
